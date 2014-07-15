ca2b925b3bfcc7cbff1780e9acd4d2552ecd84f0
Sameer Rahmani <lxsameer@gnu.org>
2014-07-16 01:36:54 +0430

    widgets js added

--- src/_assets/javascripts/widgets.js
+++ src/_assets/javascripts/widgets.js
@@ -1,15 +1,24 @@
+var API_PREFIX = "http://localhost"
 var API = {
     get: function(url) {
-        return $.ajax({url: url});
-    },
-}
-$(function(){
-    $('[data-remote="true"]').each(function(index){
+        return $.ajax({ url: API_PREFIX + url });
+    }
+};
+
+$(function() {
+    $('[data-remote="true"]').each(function(index) {
         var url = $(this).data('url');
         API.get(url)
-            .done(function(data){
+            .done(function(data) {
+                $(that).find('[data-template]').each(function(x) {
+                    var key = $(this).data('key');
+                    if (data[key] !== undefined) {
+                        $(this).html(data[key]);
+                    }
+                });
             })
-            .fail(function(data){});
-
+            .fail(function(data) {
+                alert(data);
+           });
     });
 });
