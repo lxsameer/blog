e20372ae6ed278b2b2067a3cf398764cdf43fb20
Sameer Rahmani <lxsameer@gnu.org>
2014-07-16 03:21:37 +0430

    widget api done

--- src/_assets/javascripts/widgets.js
+++ src/_assets/javascripts/widgets.js
@@ -1,24 +1,23 @@
-var API_PREFIX = "http://localhost"
+var API_PREFIX = "http://localhost:3000";
 var API = {
-    get: function(url) {
-        return $.ajax({ url: API_PREFIX + url });
+    get: function(url, callback) {
+        return $.getJSON(API_PREFIX + url + "?callback=?", callback);
     }
 };
 
 $(function() {
     $('[data-remote="true"]').each(function(index) {
         var url = $(this).data('url');
-        API.get(url)
-            .done(function(data) {
-                $(that).find('[data-template]').each(function(x) {
-                    var key = $(this).data('key');
-                    if (data[key] !== undefined) {
-                        $(this).html(data[key]);
-                    }
-                });
-            })
-            .fail(function(data) {
-                alert(data);
-           });
+        var that = this;
+        API.get(url, function(data) {
+            $(that).find('[data-template]').each(function(x) {
+                var key = $(this).data('key');
+                if (data[key] !== undefined) {
+                    $(this).html(data[key]);
+                }
+            });
+        }).fail(function(data) {
+            alert(data);
+        });
     });
 });
