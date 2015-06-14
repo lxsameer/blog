d775dddcf94337b86ef472a24c26976810e38c62
Sameer Rahmani <lxsameer@gnu.org>
2015-06-14 18:32:09 +0430

    portfolio improved

--- src/_plugins/categories.rb
+++ src/_plugins/categories.rb
@@ -17,7 +17,7 @@ module Jekyll
         posts_in_category = context.registers[:site].categories[category].size
         # <a href="#" class="item"><div class="ui purple circular label">34</div> {{ cat[0] }}</a>
 
-        html << "<div class='item'><a href='/categories/#{category}/'><div class='ui mini #{colors.sample} image label'>#{category.capitalize}<div class='detail'>#{posts_in_category}</div></div></a></div>\n"
+        html << "<div class='item'><a href='/categories/#{category}/'><div class='ui small #{colors.sample} image label'>#{category.capitalize}<div class='detail'>#{posts_in_category}</div></div></a></div>\n"
       end
       html
     end
